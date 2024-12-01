a4ffe4a116e9e7de2d7662946d372b19d8bf9f9d
bejaoui houssem <housie.bejaoui@gmail.com>
2024-12-01 23:39:10 +0100

    Updated Flutter code

--- ios/Runner/AppDelegate.swift
+++ ios/Runner/AppDelegate.swift
@@ -1,12 +1,13 @@
 import Flutter
 import UIKit
-
+import Firebase
 @main
 @objc class AppDelegate: FlutterAppDelegate {
   override func application(
     _ application: UIApplication,
     didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey: Any]?
   ) -> Bool {
+  FirebaseApp.configure()
     GeneratedPluginRegistrant.register(with: self)
 
     // Disable screenshots and screen recording
